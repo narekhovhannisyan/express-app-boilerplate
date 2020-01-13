2eccf8770d3f2d2b0a556f8093c7bf6c47c4935f
narekhovhannisyan <narekgilmour@gmail.com>
2020-01-13 17:45:27 +0400

    src: update app.js file.

--- src/app.js
+++ src/app.js
@@ -1,58 +1,34 @@
-'use strict'
+''
 
 const bodyParser = require('body-parser')
 const express = require('express')
-const session = require('express-session')
-const RedisStore = require('connect-redis')(session)
 const morgan = require('morgan')
 
-const config = require('./config/config')
+const { PathNotFoundError } = require('./util/errors.util')
 
-const models = require('./models')
-
-const { PathNotFoundError } = require('./util').Errors
-
-const healthApi = require('./routes/health/health.api')
-const api = require('./routes')
+const api = require('./api')
 
 const app = express()
 
 /**
- * @description Middleware - setup a logger.
+ * Middleware - setup a logger.
  */
 app.use(morgan('dev'))
 
 /**
- * @description Middleware - body parser:
+ * Middleware - body parser:
  * 1. Parses the text as URL encoded data (limit 5 mb).
  * 2. Parses the text as JSON & exposes the resulting object on req.body (limit 5 mb).
  */
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: false }))
 app.use(bodyParser.json({ limit: '5mb' }))
 
-/**
- * @description Middleware - session.
- */
-app.use(session({
-  key: config.COOKIE.KEY,
-  store: new RedisStore({ client: models.redis }),
-  secret: config.COOKIE.SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
-
-/**
- * @description Add health API (NO authorization, NO api prefix).
- */
-app.use('/health', healthApi)
-
-/**
- * @description Initialize APIs.
- */
 app.use('/api/v1', api)
 
+app.get('/favicon.ico', (request, response) => response.status(204))
+
 /**
- * @description Middleware - catch 404 and forward to error handler.
+ * Middleware - catch 404 and forward to error handler.
  */
 app.use((req, res, next) => next(new PathNotFoundError('The specified resource path does not exist.')))
 
