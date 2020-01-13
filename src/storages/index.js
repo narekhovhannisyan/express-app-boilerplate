ddaf0fad36db990499f0c83599c83978831e218b
narekhovhannisyan <narekgilmour@gmail.com>
2020-01-13 17:41:53 +0400

    storages: init index.js file  and add comment for further development.

---
+++ src/storages/index.js
@@ -0,0 +1,6 @@
+/**
+ * Re-export all storage for further usage with ES6 destructuring.
+ */
+// Add your favorite clients here as described below.
+// module.exports.RedisClient = require('./redis')
+// module.exports.SequelizeClient = require('./sequelize')
