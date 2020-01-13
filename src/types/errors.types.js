2ed79b1e65cdfa2628dc0cd19b466ef5125789ba
narekhovhannisyan <narekgilmour@gmail.com>
2020-01-13 17:41:22 +0400

    types: init errors.

---
+++ src/types/errors.types.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {Object} TCustomErrors
+ * @property {ErrorConstructor} InputValidationError
+ * @property {ErrorConstructor} PathNotFoundError
+ * @property {ErrorConstructor} ProcessEnvVariableError
+ */
+
+module.exports = {}
