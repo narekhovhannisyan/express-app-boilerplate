ecc1bee22bb946595096b84f24eaccb56fbe2971
narekhovhannisyan <narekgilmour@gmail.com>
2021-04-14 15:48:12 +0400

    middleware: update health validation.

--- src/middleware/validation/health.validation.js
+++ src/middleware/validation/health.validation.js
@@ -1,24 +1,27 @@
-const Joi = require('joi')
+/**
+ * @type {import('@hapi/joi')}
+ */
+const Joi = require('@hapi/joi')
 
 const validate = require('../../util/validation.util')
 
 /**
- * @typedef {import('express').Request} TRequest
- * @typedef {import('express').Response} TResponse
- * @typedef {import('express').NextFunction} TNextFunction
+ * @typedef {import('express').Request} Request
+ * @typedef {import('express').Response} Response
+ * @typedef {import('express').NextFunction} NextFunction
  */
 
 /**
- * @param {TRequest} request
- * @param {TResponse} response
- * @param {TNextFunction} next
+ * @param {Request} request
+ * @param {Response} response
+ * @param {NextFunction} next
  * @returns {void}
  */
 const validateHealthCheckArgs = (request, response, next) => {
-  validate({ },
-    {
-      // validate your data here
-    }, next)
+  validate(request, { params: request.params, request },
+    Joi.object({}),
+    next
+  )
 }
 
 module.exports = {
